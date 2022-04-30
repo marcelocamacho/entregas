3a1c7fea81dc60eec465104f6e46157cc288c0b7
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2022-04-30 16:19:01 -0300

    Criação das entidades

---
+++ prisma/migrations/20220430191548_create_client/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Deliveries" (
+    "id" TEXT NOT NULL,
+    "id_client" TEXT NOT NULL,
+    "id_delivery" TEXT NOT NULL,
+    "item_name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "end_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Deliveries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_username_key" ON "clients"("username");
