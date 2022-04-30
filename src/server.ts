24628d487bae77a754b6497e6cbf3bc7c5a363af
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2022-04-30 17:41:29 -0300

    Cadastro do cliente (UseCase e Controller); Criação do routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express';
+import { routes } from './routes';
 const app = express();
 
+app.use(express.json());
 
+app.use(routes);
 
-app.get('/',(request, response)=>{
-  return response.json({
-    message: "Hello World",
-  });
-});
-
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3333, () => console.log("Server is running"));
